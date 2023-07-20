52304e01de288a8e4b376bc953e5e6f85694fcf1
LaChaznie Moore <lachazniemoore@LaChaznies-MacBook-Air.local>
2023-07-20 13:51:45 -0400

    setup

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="header">
+      <header className="header">
+        
+        <p>
+         LaChaznie M Moore
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
